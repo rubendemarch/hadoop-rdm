10beab14f4c8a8ff01d540ee9e3d1bb219ad34dd
rubendemarch@gmail.com <rubendemarch@gmail.com@129ac649-502b-f7af-6d19-97a2811442c1>
2013-12-11 15:40:34 +0000

    Aggiunti adeguati commenti

--- trunk/HadoopProgettino/src/it/rdm/hadoop/invertIndex/InvertIndexMapper.java
+++ trunk/HadoopProgettino/src/it/rdm/hadoop/invertIndex/InvertIndexMapper.java
@@ -22,10 +22,13 @@ LongWritable, // input key type passed by TextInputFormat
             Text value,         //  input value type
             Context context) throws IOException, InterruptedException {
 
+    		//Create array with the words from input, splitted by space character
             String[] words = value.toString().split("\\s+");
             
             for (String string : words) {
+            	//remove not alphabetic characters
             	String cleanWord = string.replaceAll("[^\\p{Alpha}]", "");
+            	//export lowercased word along with the name of the file the word comes from 
             	if (!cleanWord.isEmpty()) {
 					context.write(new Text(cleanWord.toLowerCase()),new Text(((FileSplit)context.getInputSplit()).getPath().getName()));
 				}
--- trunk/HadoopProgettino/src/it/rdm/hadoop/invertIndex/InvertIndexReducer.java
+++ trunk/HadoopProgettino/src/it/rdm/hadoop/invertIndex/InvertIndexReducer.java
@@ -22,16 +22,21 @@ class InvertIndexReducer extends Reducer<
         Iterable<Text> values, // input value type
         Context context) throws IOException, InterruptedException {
 
+    	// Stringbuilder for appending filenames
         StringBuilder output = new StringBuilder();
+        // List for tracking filenames already appended
         List<String> cleanValues = new ArrayList<String>();
         for (Text fileName : values) {
     		String string = fileName.toString();
+    		// If filename isn't appeared before, it will be appended to output
         	if (!cleanValues.contains(string)){
 				output.append(string + " ");
 				cleanValues.add(string);
         	}
 		}
+        //delete exceeding last space character of String output
         output.deleteCharAt(output.length()-1);
+        
         context.write(key, new Text(output.toString()));
     }
 }
